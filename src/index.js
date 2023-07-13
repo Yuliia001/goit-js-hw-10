cf2e3cb72a174d9d502fa349169a20239dc63da5
Yuliia001 <babeyuliia@gmail.com>
2023-07-14 00:13:21 +0200

    add

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_V44W7mYW5tvarXTpNIUiUrdadDllJNHA9D11rFWWXU1xNvRvQvB553CLeAZMt2et";
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const END_POINT = '/breeds';
