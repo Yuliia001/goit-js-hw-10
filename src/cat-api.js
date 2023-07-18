9832f0f7252c30ea7cb557fbbdd4f5ffc65de672
Yuliia001 <babeyuliia@gmail.com>
2023-07-18 17:51:51 +0200

    add change

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,18 +2,33 @@ import axios from 'axios';
 
 axios.defaults.headers.common["x-api-key"] = "live_V44W7mYW5tvarXTpNIUiUrdadDllJNHA9D11rFWWXU1xNvRvQvB553CLeAZMt2et";
 const BASE_URL = 'https://api.thecatapi.com/v1';
-const END_POINT = '/breeds';
-
+const END_POINT1 = '/breeds';
+const END_POINT2 = '/images/search';
 function fetchBreeds(){
-    return axios.get(`${BASE_URL}${END_POINT}`)
-        .then(response => {
-            if (!response.status) {
+    return axios.get(`${BASE_URL}${END_POINT1}`)
+    .then(response => {
+        if (response.status !== 200) {
                 // console.log(response.status);
-            throw new Error(response.statusText);
-                
-            }
-        return response.data;
-  })
- 
+          throw new Error(response.statusText);    
+        }
+    return response.data;
+        })
+    .catch(() => {
+        Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+  });
 }
-export { fetchBreeds };
+
+function fetchCatByBreed(breedId) {
+    return axios.get(`${BASE_URL}${END_POINT2}?breed_ids=${breedId}`)
+    .then(response => {
+        if (response.status !== 200) {
+          throw new Error(response.statusText);   
+        }
+    return response.data;
+        })  
+    .catch(() => {
+        Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+  });  
+}
+
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,30 +1,53 @@
 import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
-import { fetchBreeds } from "./cat-api";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
     
 const refs = {
     select: document.querySelector('.breed-select'),
     loader: document.querySelector('.loader'),
-    error: document.querySelector('.error'),
     catInfo: document.querySelector('.cat-info')
 };
-const { select, loader, error, catInfo } = refs;
+const { select, loader, catInfo } = refs;
 
-// select.addEventListener('select', fetchBreeds)
+select.addEventListener('change', handlerSelectCat)
 
 fetchBreeds()
     .then(data => {
-        // select.innerHTML = createMarkup(data);
-        select.insertAdjacentHTML('beforeend', createMarkup(data));
-         console.log(data)
-})
-    .catch(error => {
-        console.log(error)
-    }
-  )
+        const options = createMarkup(data);
+        new SlimSelect({
+            select: select,
+            data: options
+        });
+        loader.style.display = 'none';
+    });
+   
   
-
 function createMarkup(arr) {
-    return arr.map(({id, name}) => `<option value="${id}">${name}</option>`).join('')
+    return arr.map(({ id, name }) => `<option value="${id}">${name}</option>`).join('');
+   
 }
+function handlerSelectCat(evt) {
+    loader.style.display = 'block';
+    const breedIdSelected = evt.currentTarget.value;
 
+    fetchCatByBreed(breedIdSelected) 
+        .then(data => {
+        if (!data.length) {
+            return;
+        }
+            
+        catInfo.innerHTML = createMarkupInfoCat(data)
+        loader.style.display = 'none';
+    
+    })
+}
+  
+function createMarkupInfoCat(arr) {
+    return arr.map(({ breed: [{name, description, temperament}], url }) => `
+      <img src="${url}" alt="${name}" width=300>
+      <h2>Порода: ${name} </h2>
+      <p>${description}</p>
+      <p>Темперамент: ${temperament}</p>`)
+        .join('')
+      
+}
